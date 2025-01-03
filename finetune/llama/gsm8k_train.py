0e121c201b5933e0e6a61224fe12924df35ad6e6
LuYuPromax <smyin2001@163.com>
2025-01-03 21:03:26 +0800

    集中式环境下的训练代码

---
+++ finetune/llama/gsm8k_train.py
@@ -0,0 +1,169 @@
+import copy
+import logging
+import os
+os.environ['NCCL_P2P_DISABLE'] = '1'
+os.environ['NCCL_IB_DISABLE'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+from dataclasses import dataclass, field
+from typing import Dict, Optional, Sequence
+
+import torch
+import transformers
+from torch.utils.data import Dataset
+from transformers import Trainer,TrainingArguments
+from peft import LoraConfig, TaskType, get_peft_model
+from datasets import load_dataset
+
+
+IGNORE_INDEX = -100
+DEFAULT_PAD_TOKEN = "[PAD]"
+DEFAULT_EOS_TOKEN = "</s>"
+DEFAULT_BOS_TOKEN = "<s>"
+DEFAULT_UNK_TOKEN = "<unk>"
+ANSWER_PROMPT = "The final answer is: "
+QUESTION_PROMPT = "\nAnswer the above question. First think step by step and then answer the final number.\n"
+
+
+
+def smart_tokenizer_and_embedding_resize(
+    special_tokens_dict: Dict,
+    tokenizer: transformers.PreTrainedTokenizer,
+    model: transformers.PreTrainedModel,
+):
+    """Resize tokenizer and embedding.
+
+    Note: This is the unoptimized version that may make your embedding size not be divisible by 64.
+    """
+    num_new_tokens = tokenizer.add_special_tokens(special_tokens_dict)
+    model.resize_token_embeddings(len(tokenizer))
+
+    if num_new_tokens > 0:
+        input_embeddings = model.get_input_embeddings().weight.data
+        output_embeddings = model.get_output_embeddings().weight.data
+
+        input_embeddings_avg = input_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
+        output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
+
+        input_embeddings[-num_new_tokens:] = input_embeddings_avg
+        output_embeddings[-num_new_tokens:] = output_embeddings_avg
+
+def _tokenize_fn(strings: Sequence[str], tokenizer: transformers.PreTrainedTokenizer) -> Dict:
+    """Tokenize a list of strings."""
+    tokenized_list = [
+        tokenizer(
+            text,
+            return_tensors="pt",
+            padding="max_length",
+            max_length=512,
+            truncation=True,
+        )
+        for text in strings
+    ]
+    input_ids = labels = [tokenized.input_ids[0] for tokenized in tokenized_list]
+    input_ids_lens = labels_lens = [
+        tokenized.input_ids.ne(tokenizer.pad_token_id).sum().item() for tokenized in tokenized_list
+    ]
+    return dict(
+        input_ids=input_ids,
+        labels=labels,
+        input_ids_lens=input_ids_lens,
+        labels_lens=labels_lens,
+    )
+
+class SupervisedDataset(Dataset):
+    def __init__(self, raw_data, tokenizer):
+        super(SupervisedDataset, self).__init__()
+
+        sources = [f"{example['question']}{QUESTION_PROMPT}" for example in raw_data]
+        targets = [f"{example['answer']}{tokenizer.eos_token}".replace("####", ANSWER_PROMPT) for example in raw_data]
+
+        examples = [s + t for s, t in zip(sources, targets)]
+        examples_tokenized, sources_tokenized = [_tokenize_fn(strings, tokenizer) for strings in (examples, sources)]
+        input_ids = examples_tokenized["input_ids"]
+        labels = copy.deepcopy(input_ids)
+        for label, source_len in zip(labels, sources_tokenized["input_ids_lens"]):
+            label[:source_len] = IGNORE_INDEX
+        data_dict = dict(input_ids=input_ids, labels=labels)
+        self.input_ids = data_dict["input_ids"]
+        self.labels = data_dict["labels"]
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return {"input_ids": self.input_ids[idx], "labels": self.labels[idx]}
+
+@dataclass
+class DataCollatorForSupervisedDataset(object):
+    """Collate examples for supervised fine-tuning."""
+
+    tokenizer: transformers.PreTrainedTokenizer
+
+    def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
+        input_ids, labels = tuple([instance[key] for instance in instances] for key in ("input_ids", "labels"))
+        input_ids = torch.nn.utils.rnn.pad_sequence(
+            input_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id
+        )
+        labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=IGNORE_INDEX)
+        return dict(
+            input_ids=input_ids,
+            labels=labels,
+            attention_mask=input_ids.ne(self.tokenizer.pad_token_id),
+        )
+
+def make_supervised_data_module(tokenizer: transformers.PreTrainedTokenizer) -> Dict:
+    dataset = load_dataset('/home/ubuntu/smyin/datasets/gsm8k', "main")
+    #train_set = dataset['train'].select(range(500))
+    train_set = dataset['train']
+    train_dataset = SupervisedDataset(raw_data=train_set, tokenizer=tokenizer)
+    data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
+    return dict(train_dataset=train_dataset, eval_dataset=None, data_collator=data_collator)
+
+
+def train():
+    output_dir = "./llama_lora_gsm8k3"
+
+    model = transformers.AutoModelForCausalLM.from_pretrained('/home/ubuntu/smyin/models/Llama-3.2-1B-Instruct')
+
+    peft_config = LoraConfig(
+        task_type=TaskType.CAUSAL_LM,
+        r=8,               # 矩阵低秩近似的秩
+        lora_alpha=32,     
+        lora_dropout=0.1,  # LoRA 的 dropout 概率
+    )
+    model = get_peft_model(model, peft_config)
+
+    tokenizer = transformers.AutoTokenizer.from_pretrained('/home/ubuntu/smyin/models/Llama-3.2-1B-Instruct')
+
+    special_tokens_dict = dict()
+    if tokenizer.pad_token is None:
+        special_tokens_dict["pad_token"] = DEFAULT_PAD_TOKEN
+    if tokenizer.eos_token is None:
+        special_tokens_dict["eos_token"] = DEFAULT_EOS_TOKEN
+    if tokenizer.bos_token is None:
+        special_tokens_dict["bos_token"] = DEFAULT_BOS_TOKEN
+    if tokenizer.unk_token is None:
+        special_tokens_dict["unk_token"] = DEFAULT_UNK_TOKEN
+
+    smart_tokenizer_and_embedding_resize(special_tokens_dict=special_tokens_dict,tokenizer=tokenizer,model=model,)
+
+    data_module = make_supervised_data_module(tokenizer=tokenizer)
+
+    training_args = TrainingArguments(
+        output_dir=output_dir,
+        per_device_train_batch_size=5,
+        gradient_accumulation_steps=4,
+        num_train_epochs=5,
+        save_steps=500,
+        logging_steps=200,
+        evaluation_strategy="no",
+    )
+
+    trainer = Trainer(model=model, tokenizer=tokenizer, args=training_args, **data_module)
+    trainer.train()
+    trainer.save_state()
+    trainer.save_model(output_dir)
+
+
+if __name__ == "__main__":
+    train()
